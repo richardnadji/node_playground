ceeca13f81aaebbfc90fe75ed7a1df3c35118957
Richard Nadji <richardnadji@hotmail.com>
2017-04-10 01:23:14 +0200

    add try catch error for request

--- app.js
+++ app.js
@@ -11,27 +11,31 @@ printMessage = (username, badgeCount, points) => {
 
 //STEPS:
 getProfile = username => {
-  //Connect to the API URL (https://teamtreehouse.com/username.json)
-  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    let body = "";
-    //Read the data
-    response.on('data', data => {
-      body += data.toString();
-    });
+  try {
+    //Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+      let body = "";
+      //Read the data
+      response.on('data', data => {
+        body += data.toString();
+      });
 
-    response.on('end', () => {
-      //Parse the data
-      const profile = JSON.parse(body);
-      // console.log(profile);
-      //Print the data
-      printMessage(username, profile.badges.length, profile.points.JavaScript);
-      // console.log(body);
-      // console.log(typeof body);
+      response.on('end', () => {
+        //Parse the data
+        const profile = JSON.parse(body);
+        // console.log(profile);
+        //Print the data
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+        // console.log(body);
+        // console.log(typeof body);
+      });
     });
-  });
-  request.on('error', error => {
-    console.error(error);
-  });
+    request.on('error', error => {
+      console.error(`Problem with request: ${error.message}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // console.dir(process.argv);
