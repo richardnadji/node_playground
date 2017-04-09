52c0a683d90ad3591d5522bce40a132f9a69065c
Richard Nadji <richardnadji@hotmail.com>
2017-04-10 01:06:15 +0200

    add getProfile function and made possible to add usernames from commandline

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 //Solution: use node.js to connect to teamtreehouse's API, get profile information to print out
 
 const https = require('https');
-const username = "richardnadji"
 
 printMessage = (username, badgeCount, points) => {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in javascript.`;
@@ -10,27 +9,36 @@ printMessage = (username, badgeCount, points) => {
 }
 
 //STEPS:
-//Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-  let body = "";
-  //Read the data
-  response.on('data', data => {
-    body += data.toString();
+getProfile = username => {
+  //Connect to the API URL (https://teamtreehouse.com/username.json)
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    let body = "";
+    //Read the data
+    response.on('data', data => {
+      body += data.toString();
+    });
+
+    response.on('end', () => {
+      //Parse the data
+      const profile = JSON.parse(body);
+      // console.log(profile);
+      //Print the data
+      printMessage(username, profile.badges.length, profile.points.JavaScript);
+      // console.log(body);
+      // console.log(typeof body);
+    });
   });
-
-  response.on('end', () => {
-    //Parse the data
-    const profile = JSON.parse(body);
-    // console.log(profile);
-    //Print the data
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
-    // console.log(body);
-    // console.log(typeof body);
+  request.on('error', error => {
+    console.error(error);
   });
+}
 
+// console.dir(process.argv);
 
-});
+// const users = ["richardnadji", "chalkers"];
+// users.forEach(user => {
+//   getProfile(user);
+// })
 
-request.on('error', error => {
-  console.error(error);
-});
+const users = process.argv.slice(2);
+users.map(getProfile);
